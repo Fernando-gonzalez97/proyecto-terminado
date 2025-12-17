9476742f5406b106280c5734c678d6b5430ae432
fernando <fernag97@gmail.com>
2025-12-17 20:34:44 -0300

    ALLOWED_HOSTS actualizado para producción

--- Proyecto_Final-main/final/final/settings.py
+++ Proyecto_Final-main/final/final/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k=(sfj0_b1hfc33iu=82bzz^!!#tg^2l2f13tisu=qhpyf88wl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fernandogonzalezz97.pythonanywhere.com']
 
 
 # Application definition
